c52c2b8ae3c55949abb00898e6c0060ddfbd9686
System Administrator <root@shuheinoMacBook-Pro.local>
2022-10-27 16:34:03 +0900

    finish Rendor Prompt

--- rust_bash/target/debug/deps/rust_bash-903d29d8a52f6c89.d
+++ rust_bash/target/debug/deps/rust_bash-903d29d8a52f6c89.d
@@ -1,5 +1,6 @@
-/Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/target/debug/deps/rust_bash-903d29d8a52f6c89: src/main.rs
+/Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/target/debug/deps/rust_bash-903d29d8a52f6c89: src/main.rs src/event.rs
 
-/Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/target/debug/deps/rust_bash-903d29d8a52f6c89.d: src/main.rs
+/Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/target/debug/deps/rust_bash-903d29d8a52f6c89.d: src/main.rs src/event.rs
 
 src/main.rs:
+src/event.rs:
--- rust_bash/target/debug/rust_bash.d
+++ rust_bash/target/debug/rust_bash.d
@@ -1,1 +1,1 @@
-/Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/target/debug/rust_bash: /Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/src/main.rs
+/Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/target/debug/rust_bash: /Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/src/event.rs /Users/s_koni/work_dir/rust/rust_bash_20221027/rust_bash/src/main.rs
